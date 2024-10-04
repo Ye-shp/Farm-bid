a97aa036b68685fd817cae1f3fe598be4db00dbe
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-10-04 19:48:06 -0400

    .

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
-dotenv.config();
+require('dotenv').config(); 
+ 
 
 const app = express();
 app.use(express.json());
