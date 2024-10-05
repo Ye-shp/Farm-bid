ee11fc8af1f97b459978f60907e914b610fbc270
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-10-04 20:25:14 -0400

    Mongo uri

--- app.js
+++ app.js
@@ -37,9 +37,7 @@ app.get('/', (req, res) => {
 // Connect to MongoDB with enhanced error handling
 console.log('MONGO_URI:', process.env.MONGO_URI);
 
-//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connect('mongodb+srv://yeabsiramesfin21:Ab366544@cluster0.cu0iwq2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => {
     console.error('MongoDB connection error:', err.message);
