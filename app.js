4fb7735da7730b2687d47ce0cc168bb2290c5fe7
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-10-06 18:58:45 -0400

    changed port env

--- app.js
+++ app.js
@@ -66,5 +66,5 @@ process.on('unhandledRejection', (reason, promise) => {
   process.exit(1); // Exit process with failure
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
