49f54535d3b7340fdb920270d9a6634023c631ca
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-10-24 17:50:27 -0400

    Used wrong name for middleware

--- routes/notificationRoute.js
+++ routes/notificationRoute.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { getNotifications } = require('../controllers/auctionControllers');
-const auth = require('../middleware/auth');
+const auth = require('../middleware/authMiddleware');
 
 router.get('/', auth, getNotifications);
 
