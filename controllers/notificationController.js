5df0c602550d1bf1799a45a6b1a6c4d3f8accf09
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-10-25 23:02:55 -0400

    Wont matter

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,4 +1,6 @@
 // In a new or existing notifications controller
+const Notification = require('../models/Notification');
+
 exports.getNotifications = async (req, res) => {
     try {
       const notifications = await Notification.find({ user: req.user.id }).sort({ createdAt: -1 });
