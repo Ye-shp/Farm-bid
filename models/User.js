670d0d8597efaada38a9de62508cc129fa030ca9
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-10-30 22:50:56 -0400

    added api endpoint for useblog retrieval

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true, enum: ['farmer', 'buyer'] },
+  blogs: [{type: mongoose.Schema.Types.ObjectId, ref:'Blog'}],
   
   location: {
     latitude: { type: Number, required: false },
