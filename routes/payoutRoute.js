f8a4e207332b939ba8bf0fe8448687204c33ae76
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-11-07 18:20:35 -0500

    Added account linking for payouts.

--- routes/payoutRoute.js
+++ routes/payoutRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const payoutController = require('../controllers/payoutController');
 
 router.post('/create-payout', payoutController.createPayout);
+router.post('/create-connected-account',payoutController.createConnectedAccount);
 
 module.exports = router;
