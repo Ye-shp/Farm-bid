199b0b23074b9c8a4944be954b05ac341218b0e4
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-11-30 02:37:56 -0500

    fixed i hope

--- controllers/contractController.js
+++ controllers/contractController.js
@@ -2,6 +2,12 @@
 const OpenContract = require('../models/OpenContract');
 const twilio = require ('twilio');
 
+
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+
 const client = twilio(accountSid, authToken);
 
 // Create a new open contract (for buyers)
