854131d18a8ea6e8e3ff8043cb3ffe828bfa9fd7
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-11-30 00:58:46 -0500

    Added validator for phone numbers

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require ('validator');
 
 const UserSchema = new mongoose.Schema({
   username: {type: String, required: true, unique: true},
