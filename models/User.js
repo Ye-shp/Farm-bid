6169aca6e32be0284e5d9198eb5017fe1e13a456
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-02-09 18:45:31 +0800

    added missing deliveryAvailable field

--- models/User.js
+++ models/User.js
@@ -82,6 +82,7 @@ const UserSchema = new mongoose.Schema({
   ],
 
   wholesaleAvailable: { type: Boolean, default: false },
+  deliveryAvailable: { type: Boolean, default: false },
   followers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
   following: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
 
