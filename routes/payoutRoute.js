a33a13d1643d42f11b36366551e13b888580d9b8
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2025-02-24 18:40:22 -0500

    routes for payout

--- routes/payoutRoute.js
+++ routes/payoutRoute.js
@@ -1,13 +1,14 @@
-// routes/payoutRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const payController = require('../controllers/payController');
 
+
+router.get('/seller-balance', payController.getSellerBalance);
+router.get('/seller-transfers', payController.getSellerTransfers);
+
 router.post('/create-payout', payController.createPayout);
-router.post('/create-connected-account',payController.createConnectedAccount);
+router.post('/create-connected-account', payController.createConnectedAccount);
 router.post('/add-bank-account', payController.addBankAccount);
 router.post('/create-payout-for-auction', payController.createPayoutForAuction);
-
 
 module.exports = router;
