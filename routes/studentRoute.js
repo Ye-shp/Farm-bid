54c6eafc8c8da99149056bd7bd4c7e652d41768c
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2025-03-03 02:31:48 -0500

    More spelling errors

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Student = require('../models/Student');
+const Student = require('../models/Students');
 const jwt = require('jsonwebtoken');
 
 // Student Registration
